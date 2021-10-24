8d2e951d38b3a1beedd03d93ebfb3d5bb5aad7c2
Kiran Chhablani <83265750+kiranchhablani@users.noreply.github.com>
2021-10-24 09:08:35 +0530

    Update MyContract.sol
    final commit

--- MyContract.sol
+++ MyContract.sol
@@ -1,2 +1,30 @@
-pragma solidity ^0.4.24;
- 
+pragma solidity ^0.5.1;
+
+contract MyContract {
+    enum State{Waiting, Ready, Active}
+    State public state;
+    
+    constructor() public{
+        state = State.Waiting;
+    }
+    
+    function activate() public{
+        state = State.Active;
+    }
+    
+    function ready() public{
+        state = State.Ready;
+    }
+    
+    function isActive() public view returns(bool){
+        return state == State.Active;
+    }
+    
+    function isReady() public view returns(bool){
+        return state == State.Ready;
+    }
+    
+    function isWaiting() public view returns(bool){
+        return state == State.Waiting;
+    }
+}
